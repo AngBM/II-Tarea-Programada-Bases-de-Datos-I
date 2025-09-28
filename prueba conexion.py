cfe4542a9805eee0283d6c9beefc136ea4d55287
catherinmb2019-tech <catherinmb2019@gmail.com>
2025-09-27 23:27:01 -0600

    login pagina

---
+++ prueba conexion.py
@@ -0,0 +1,17 @@
+import pyodbc
+
+
+connection = pyodbc.connect(
+    "DRIVER={ODBC Driver 18 for SQL Server};"
+    "SERVER=tcp:servidorbdcmb19.database.windows.net,1433;"
+    "DATABASE=Tarea 2 BD;"
+    "UID=AdminCMB19;"
+    "PWD=adminbdCMB05;"
+    "Encrypt=yes;"
+    "TrustServerCertificate=no;"
+    "Connection Timeout=30;"
+)
+
+print(" Conexión exitosa")
+cursor = connection.cursor()
+cursor.execute("")
