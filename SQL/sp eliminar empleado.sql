9465bc80e37d64e6d1ec532a4311c09f0ff08809
AngBM <113566708+AngBM@users.noreply.github.com>
2025-10-18 00:17:10 -0600

    Create sp eliminar empleado.sql
    sp encargado de eliminar empleado

---
+++ SQL/sp eliminar empleado.sql
@@ -0,0 +1,26 @@
+CREATE PROCEDURE sp_eliminar_empleado
+    @ValorDocumentoIdentidad NVARCHAR(20),
+    @Confirmado BIT,
+    @PostByUser NVARCHAR(50),
+    @PostInIP NVARCHAR(50)
+AS
+BEGIN
+    SET NOCOUNT ON;
+    DECLARE @IdUser INT, @Nombre NVARCHAR(100), @Puesto NVARCHAR(100), @Saldo DECIMAL(10,2);
+    SELECT @IdUser = Id FROM Usuario WHERE Username = @PostByUser;
+    SELECT @Nombre = E.Nombre, @Puesto = P.Nombre, @Saldo = E.SaldoVacaciones
+    FROM Empleado E JOIN Puesto P ON E.IdPuesto = P.Id
+    WHERE E.ValorDocumentoIdentidad = @ValorDocumentoIdentidad;
+
+    IF @Confirmado = 0
+    BEGIN
+        INSERT INTO BitacoraEvento VALUES(10, CONCAT(@ValorDocumentoIdentidad, ', ', @Nombre, ', ', @Puesto, ', ', @Saldo),
+                                          @IdUser, @PostInIP, GETDATE());
+        RETURN;
+    END
+
+    DELETE FROM Empleado WHERE ValorDocumentoIdentidad = @ValorDocumentoIdentidad;
+    INSERT INTO BitacoraEvento VALUES(8, CONCAT(@ValorDocumentoIdentidad, ', ', @Nombre, ', ', @Puesto, ', ', @Saldo),
+                                      @IdUser, @PostInIP, GETDATE());
+END
+GO
