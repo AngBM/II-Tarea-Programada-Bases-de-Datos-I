bf93cb66802e8560fb1fa0fe03cc473678024519
AngBM <113566708+AngBM@users.noreply.github.com>
2025-10-18 23:44:51 -0600

    Create sp insertar bitacora.sql

---
+++ SQL/sp insertar bitacora.sql
@@ -0,0 +1,15 @@
+CREATE PROCEDURE sp_insertar_bitacora
+    @TipoEvento NVARCHAR(50),
+    @Descripcion NVARCHAR(500),
+    @IdPostByUser NVARCHAR(50),
+    @IP NVARCHAR(50)
+AS
+BEGIN
+    INSERT INTO BitacoraEvento(IdTipoEvento, Descripcion, IdPostByUser, PostInIP, PostTime)
+    VALUES ((SELECT Id FROM TipoEvento WHERE Nombre = @TipoEvento),
+            @Descripcion, 
+            (SELECT Id FROM Usuario WHERE Username = @IdPostByUser),
+            @IP,
+            GETDATE());
+END
+GO
